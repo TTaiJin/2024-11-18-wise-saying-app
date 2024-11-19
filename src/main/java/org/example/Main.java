eb8ff7437075639a71225c2c7fb8c5c8122c6a2a
TTaiJin <taeparis980110@gmail.com>
2024-11-19 14:25:54 +0900

    App Class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,6 +16,11 @@ class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
+        Scanner scanner = new Scanner(System.in);
+
         System.out.print("명령) ");
+        String cmd = scanner.nextLine();
+
+        System.out.println("입력된 명령어 : %s".formatted(cmd));
     }
 }
