2b89149c46ec2dc10f186d37fa1d4fe974c6f6fb
TTaiJin <taeparis980110@gmail.com>
2024-11-19 14:11:10 +0900

    app class

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.run();
+    }
+
+}
+
+class App {
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+
+        System.out.print("명령) ");
+    }
+}
