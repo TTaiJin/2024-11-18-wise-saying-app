19698189f3f2d400fdaab4b04bf2e595010be45f
TTaiJin <taeparis980110@gmail.com>
2024-11-25 21:37:07 +0900

    Scanner App -> Controller

--- src/main/java/org/example/step9/App.java
+++ src/main/java/org/example/step9/App.java
@@ -1,22 +1,19 @@
 package org.example.step9;
 
 import org.example.step9.domain.wiseSaying.controller.WiseSayingController;
-import org.example.step9.domain.wiseSaying.entity.WiseSaying;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class App {
     private final Scanner scanner;
-    private final List<WiseSaying> wiseSayings;
     private boolean isRunning;
 
     private final WiseSayingController wiseSayingController;
 
     public App() {
         scanner = new Scanner(System.in);
-        wiseSayings = new ArrayList<>();
         isRunning = true;
-        wiseSayingController = new WiseSayingController();
+        wiseSayingController = new WiseSayingController(scanner);
     }
 
     public void run() {
@@ -30,20 +27,20 @@ public class App {
                 continue;
             }
             switch (command) {
-                case 등록 -> wiseSayingController.actionAdd(scanner, wiseSayings);
+                case 등록 -> wiseSayingController.actionAdd();
                 case 삭제 -> {
                     System.out.print("삭제할 명언의 번호를 입력하세요: ");
                     int deleteId = scanner.nextInt();
                     scanner.nextLine();
-                    wiseSayingController.actionDelete(wiseSayings, deleteId);
+                    wiseSayingController.actionDelete(deleteId);
                 }
                 case 수정 -> {
                     System.out.println("수정할 명언의 번호를 입력하세요: ");
                     int editId = scanner.nextInt();
                     scanner.nextLine();
-                    wiseSayingController.actionEdit(scanner, wiseSayings, editId);
+                    wiseSayingController.actionEdit(editId);
                 }
-                case 목록 -> wiseSayingController.actionList(wiseSayings);
+                case 목록 -> wiseSayingController.actionList();
                 case 종료 -> actionExit();
             }
         }
--- src/main/java/org/example/step9/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/org/example/step9/domain/wiseSaying/controller/WiseSayingController.java
@@ -2,15 +2,20 @@ package org.example.step9.domain.wiseSaying.controller;
 
 import org.example.step9.domain.wiseSaying.entity.WiseSaying;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
+import java.util.*;
 
 public class WiseSayingController {
+    private final Scanner scanner;
+    private final List<WiseSaying> wiseSayings;
     private int lastId = 1;
 
-    public void actionAdd(Scanner scanner, List<WiseSaying> wiseSayings) {
+    public WiseSayingController(Scanner scanner) {
+        this.scanner = scanner;
+        this.wiseSayings = new ArrayList<>();
+        this.lastId = 1;
+    }
+
+    public void actionAdd() {
         System.out.print("명언 : ");
         String content = scanner.nextLine();
         System.out.print("작가 : ");
@@ -21,7 +26,7 @@ public class WiseSayingController {
         lastId++;
     }
 
-    public void actionList(List<WiseSaying> wiseSayings) {
+    public void actionList() {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("------------------");
         wiseSayings.stream()
@@ -29,7 +34,7 @@ public class WiseSayingController {
                 .forEach(e -> System.out.println(e.getId() + " / " + e.getWriter() + " / " + e.getContent()));
     }
 
-    public void actionDelete(List<WiseSaying> wiseSayings, int deleteId) {
+    public void actionDelete(int deleteId) {
 
         boolean delete = wiseSayings.removeIf(e -> e.getId() == deleteId);
 
@@ -37,7 +42,7 @@ public class WiseSayingController {
         else System.out.println(deleteId + "번 명언이 존재하지 않습니다.");
     }
 
-    public void actionEdit(Scanner scanner, List<WiseSaying> wiseSayings, int editId) {
+    public void actionEdit(int editId) {
         Optional<WiseSaying> optionalWiseSaying = wiseSayings.stream()
                 .filter(e -> e.getId() == editId)
                 .findFirst();
@@ -54,34 +59,5 @@ public class WiseSayingController {
         } else {
             System.out.println(editId + "번 명언이 존재하지 않습니다.");
         }
-
-     /*   WiseSaying foundWiseSaying = null;
-
-        System.out.println("수정할 명언의 번호를 입력하세요: ");
-        int editId = scanner.nextInt();
-        scanner.nextLine();
-        if(editId < 0 || editId >= wiseSayings.size()) {
-            System.out.println(editId + "번 명언이 존재하지 않습니다.");
-            return;
-        }
-        for (WiseSaying wiseSaying : wiseSayings) {
-            if(wiseSaying.getId() == editId){
-                foundWiseSaying = wiseSaying;
-                break;
-            }
-        }
-        foundWiseSaying = wiseSayings.get(editId);
-
-
-        if (foundWiseSaying == null) {
-            System.out.println(editId + "번 명언이 존재하지 않습니다.");
-        } else {
-            System.out.println("명언(기존) : " + foundWiseSaying.getContent());
-            System.out.print("명언 : ");
-            foundWiseSaying.setContent(scanner.nextLine());
-            System.out.println("작가(기존) : " + foundWiseSaying.getWriter());
-            System.out.print("작가 : ");
-            foundWiseSaying.setWriter(scanner.nextLine());
-        }*/
     }
 }
